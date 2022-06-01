176eca56b21d9dd156902884b8efa2ee2994be0d
Jason Petrie <40582783+JayP232@users.noreply.github.com>
2022-06-01 14:16:00 +0200

    Updated Script
    Script had issues holding the temp list in memory, therefore the users are now loaded into a local file and the loop iterations call the values from the file instead of memory.

--- runyar.ps1
+++ runyar.ps1
@@ -1,4 +1,5 @@
-﻿$tempList = get-childitem -path C:\Users -directory | where {$_.PsIsContainer} | Select-Object Name
+$tempList = get-childitem -path C:\Users -directory | where {$_.PsIsContainer} | Select-Object Name
+echo $tempList.Name >> UserOutput.txt
 $p1 = "C:\Users\"
 $p3 = "\AppData\Roaming"
 $fomat = "Results for : "
@@ -7,19 +8,14 @@ $y2 = " against Scan 2"
 $y3 = " against Scan 3"
 $y4 = " against Scan 4"
 
-ForEach ($user in $tempList) {
-    $path =  ("C:\Users\" + $user.Name + $p3)
-    $usr = $user.Name
-
-    echo $foreach$usr$y1 >> results.txt
-    .\yara64.exe -c -r .\rOne.yar $path >> .\results.txt
-
-    echo $foreach$usr$y2 >> results.txt
-    .\yara64.exe -c -r .\rTwo.yar $path >> .\results.txt
-
+ForEach ($line in Get-Content .\UserOutput.txt) {
+     echo $fomat$line >> results.txt
+    .\yara64.exe -c -r .\rOne.yar $line >> .\results.txt
+    
     echo $foreach$usr$y3 >> results.txt
-    .\yara64.exe -c -r .\rThree.yar $path >> .\results.txt
+    .\yara64.exe -c -r .\rThree.yar $line >> .\results.txt
     
     echo $foreach$usr$y4 >> results.txt
-    .\yara64.exe -c -r .\rfour.yar $path >> .\results.txt
+    .\yara64.exe -c -r .\rfour.yar $line >> .\results.txt
 }
+ 
